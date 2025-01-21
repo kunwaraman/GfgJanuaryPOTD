9c88fe04f53ec3745e3356eabb6b173619b15591
kunwaraman <amankunwar8472@gmail.com>
2025-01-21 22:23:28 +0530

    ll (revisit)

---
+++ src/ReverseNodeInKGroups.java
@@ -0,0 +1,42 @@
+public class ReverseNodeInKGroups {
+    // ps:https://www.geeksforgeeks.org/problems/reverse-a-linked-list-in-groups-of-given-size/1
+    class Solution {
+        public static Node reverseKGroup(Node head, int k) {
+            // code here
+            if(head==null || head.next==null) return head;
+            Node t=head,n=null,p=null;
+            while(t!=null){
+                int l=1;
+                while(t.next!=null && l<k){
+                    t=t.next;
+                    l++;
+                }
+                if(p!=null) p.next=t;
+                if(n==null){
+                    p=head;
+                    head=t;
+                }
+                else p=n;
+                n=t.next;
+                t.next=null;
+                reverse(p);
+                p.next=n;
+                t=n;
+            }
+            return head;
+        }
+        static Node reverse(Node head){
+            if(head==null || head.next==null) return head;
+            Node t=head,p=null;
+            while(t!=null){
+                Node n =t.next;
+                t.next=p;
+                p=t;
+                t=n;
+            }
+            return p;
+        }
+
+    }
+
+}
