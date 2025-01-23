992f8ee15b61b888194935acdd03abcb8ea56a39
kunwaraman <amankunwar8472@gmail.com>
2025-01-24 00:22:30 +0530

    day24 ddcy

---
+++ src/DetectLoopInLinkedList.java
@@ -0,0 +1,15 @@
+public class DetectLoopInLinkedList {
+    public static boolean detectLoop(Node head) {
+        // Add code here
+        Node slow=head;
+        Node fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+}
